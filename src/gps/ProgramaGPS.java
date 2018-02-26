0429b87e0230dedc260caeacbb0a12443603d035
DanielBarria <barriareyesjosedaniel@gmail.com>
2018-02-26 11:17:17 -0300

    Agregando rutas y ciudades al grafo

--- src/gps/ProgramaGPS.java
+++ src/gps/ProgramaGPS.java
@@ -1,24 +1,27 @@
 package gps;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import configuracion.CargaXML;
 import puntos.*;
 import rutas.*;
+import grafo.*;
 
 public class ProgramaGPS {
 
 	public static void main(String[] args) {
 		//lista de ciudades y puntos
-		Punto.getListaCiudades();
+		//Punto.getListaCiudades();
 		 //lista de rutas
-		Ruta.getListaRutas();
+		//Ruta.getListaRutas();
 		 //lista de coincidencias
-		 ArrayList<Ruta> matches = new ArrayList<Ruta>();
-		 
+		 //ArrayList<Ruta> matches = new ArrayList<Ruta>();
+		 List<Vertice> puntos = new ArrayList<Vertice>();
+		 List<Arista> caminos = new ArrayList<Arista>();
 		 //los puntos y rutaas se generan por archivos xml
-		 CargaXML.abrirXMLRutas();
-		 CargaXML.abrirXMLPuntos();
+		 //CargaXML.abrirXMLRutas();
+		 //CargaXML.abrirXMLPuntos();
 		 //matches = Ruta.existeRuta(listaRutas, esquel,trelew);
 		 //Ruta.tiempoEnLlegar(listaRutas, trelew, esquel);
 		 //matches.forEach(item->System.out.println(item.toString()));
@@ -27,6 +30,12 @@ public class ProgramaGPS {
 		 
 		 //CargaXML.abrirXMLRutas();
 		 //matches.forEach(item->System.out.println(item.toString()));
+		 CargaXML.cargarXMLPuntos(puntos);
+		 CargaXML.cargarXMLAristas(caminos);
+		 //puntos.forEach(item->System.out.println(item.toString()));
+		 caminos.forEach(tiem->System.out.println(tiem.toString()+" "+tiem.getDestination().getName()
+				 + tiem.getSource().getId()
+				 ));
 		 
 	}
 
